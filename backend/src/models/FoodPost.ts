2607fd8913d140bd9f75256f228e6a496d25f511
vitu <vitu@cisco.com>
2020-05-09 12:21:25 -0700

    First commit

---
+++ backend/src/models/FoodPost.ts
@@ -0,0 +1,17 @@
+// export interface TodoItem {
+//   userId: string
+//   todoId: string
+//   createdAt: string
+//   name: string
+//   dueDate: string
+//   done: boolean
+//   attachmentUrl?: string
+// }
+
+export interface FoodagramPost {
+  userId: string
+  postId: string
+  createdAt: string
+  caption: string
+  attachmentUrl: string
+}
---
+++ client/src/config.ts
@@ -0,0 +1,7 @@
+export const apiEndpoint = 'https://rigaq6orbf.execute-api.us-east-1.amazonaws.com/dev'
+
+export const authConfig = {
+  domain: 'dev-ls5u-uhh.auth0.com',
+  clientId: 'fGc3mrpqU30HEc0wqk8ZxrfTHn8LYDEB',
+  callbackUrl: 'http://localhost:3000/callback'
+}
